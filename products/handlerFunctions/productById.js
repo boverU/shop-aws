76409fd555d35c53616c8730ff17d1987e1db5c1
Zhakizhanov <bover199131@gmail.com>
2022-07-27 11:03:24 +0600

    feat: connect with database

--- products/handlerFunctions/productById.js
+++ products/handlerFunctions/productById.js
@@ -1,26 +1,46 @@
-const fs = require('fs').promises;
+import DBPool from './dbconfig'
 
-export const productById = async (event) => {
-    const allData = await fs.readFile('./mockData.json');
-    const found = (JSON.parse(allData.toString())).find(product => product.id === Number(event.pathParameters.id));
-    if (found) {
+export const productById = async (event, context) => {
+    try {
+        const { rows } = await DBPool.query(
+            {
+                name: 'get product',
+                text: 'SELECT product.id,product.title, product.description, product.price , stock.count FROM stock INNER JOIN product ON stock.product_id = product.id where product.id=$1',
+                values: [event.pathParameters.id],
+            }
+        )
+
+
+        if (rows.length > 0) {
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                body: JSON.stringify(rows[0])
+            }
+        }
         return {
-            statusCode: 200,
+            statusCode: 404,
             headers: {
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
             },
-            body: JSON.stringify(found)
+            body: `Product under id ${event.pathParameters.id} not found`
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: `${error.stack}`
         }
     }
-    return {
-        statusCode: 404,
-        headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: `Product under id ${event.pathParameters.id} not found`
-    }
+
 }
--- products/handlerFunctions/products.js
+++ products/handlerFunctions/products.js
@@ -1,14 +1,27 @@
-const fs = require('fs').promises;
+import DBPool from './dbconfig'
 
 export const products = async (_event) => {
-    const data = await fs.readFile('./mockData.json')
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: data.toString()
-    };
+    try {
+        const result = await DBPool.query('SELECT product.id,product.title, product.description, product.price , stock.count FROM stock INNER JOIN product ON stock.product_id = product.id')
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify(result.rows)
+        };
+    } catch (error) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: error.stack
+        };
+    }
+
 };
