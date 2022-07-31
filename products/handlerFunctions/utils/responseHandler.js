3a8ccf994d25a6310b6dacd7489332f1021cd9b9
Zhakizhanov <bover199131@gmail.com>
2022-07-31 23:36:03 +0600

    feat: add options method and fix cors issue

--- products/handlerFunctions/utils/responseHandler.js
+++ products/handlerFunctions/utils/responseHandler.js
@@ -2,9 +2,9 @@ export function responseHandler(statusCode, body) {
     return {
         statusCode,
         headers: {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST,GET,DELETE,PUT"
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+            "Access-Control-Allow-Origin": "https://d3jh9f0toashuf.cloudfront.net",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,DELETE,PUT"
         },
         body
     };
